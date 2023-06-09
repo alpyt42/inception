15e3baccf65d95017485101a99777f66e01dcfa4
AlricPerso <alric.conte@gmail.com>
2023-06-09 19:08:00 +0200

    up

--- inceother/srcs/requirements/nginx/tools/nginx_start.sh
+++ inceother/srcs/requirements/nginx/tools/nginx_start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Check if SSL certificate file exists
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=FR/L=Lyon/O=42/OU=ale-cont/CN=ale-cont.42.fr";
-echo "Nginx: ssl is set up!";
+	echo "Nginx: setting up SSL certificate..."
+
+	# Generate a self-signed SSL certificate
+	openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+	-keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt \
+	-subj "/C=FR/L=Lyon/O=42/OU=ale-cont/CN=ale-cont.42.fr"
+
+	echo "Nginx: SSL certificate is set up!"
 fi
 
+# Execute the command-line arguments passed to the script
 exec "$@"
--- inceother/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ inceother/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,24 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
-	mkdir -p /run/php/;
-	touch /run/php/php7.3-fpm.pid;
+set -e
 
-if [ ! -f /var/www/html/wp-config.php ]; then
-	echo "Wordpress: setting up..."
-	mkdir -p /var/www/html
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
-	chmod +x wp-cli.phar; 
-	mv wp-cli.phar /usr/local/bin/wp;
-	cd /var/www/html;
-	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
-	echo "Wordpress: creating users..."
-	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
-	echo "Wordpress: set up!"
+if [ ! -f ./wp-config.php ]; then
+    curl -O https://wordpress.org/latest.tar.gz
+    tar -xzvf latest.tar.gz
+    mv wordpress/* .
+    rm -rf latest.tar.gz
+    rmdir wordpress
+
+    sed -i "s/username_here/$WP_USER_LOGIN/g" wp-config-sample.php
+    sed -i "s/password_here/$WP_USER_PASSWORD/g" wp-config-sample.php
+    sed -i "s/localhost/$WP_URL/g" wp-config-sample.php
+    sed -i "s/database_name_here/$WP_TITLE/g" wp-config-sample.php
+    
+    mv wp-config-sample.php wp-config.php
 fi
+
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g" /etc/php/7.3/fpm/pool.d/www.conf
 
 exec "$@"
